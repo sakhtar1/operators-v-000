1165402f132b3bd0057598dd1cc2e5987f490f5f
Sharmina Akhtar <sakht318@gmail.com>
2017-11-09 05:04:51 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-	speed > 40 && speed < 60 ? True : False
+	if speed > 40 && speed < 60 
+		return true 
+	else
+		return false
 end
 
 
 
 def not_safe?(speed)
-	speed > 40 && speed < 60 ? True : False
+	speed > 40 && speed < 60 ? true : false
 end
